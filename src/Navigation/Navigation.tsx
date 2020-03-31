433d45de4ddce80210e491ac8a751bef77f4f51e
thomas.kloppe <thomas.kloppe@sidion.de>
2020-03-31 15:14:17 +0200

    about text

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -17,11 +17,11 @@ export class Navigation extends React.Component<IProps, IState> {
 
     constructor(props: any) {
         super(props);
-        this.state = { 
+        this.state = {
             showImpressum: false,
             showDsgvo: false,
             showAbout: false
-         };
+        };
     }
 
     toggleImpressum(): void {
@@ -59,38 +59,61 @@ export class Navigation extends React.Component<IProps, IState> {
                         </NavItem>
                     </Nav>
                 </Collapse>
-                <InformationModal 
-                    open={this.state.showImpressum} 
+                <InformationModal
+                    open={this.state.showImpressum}
                     toggle={() => this.toggleImpressum()}
                     headline="Impressum">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        
-                        <p>Duis aute irure dolor in reprehenderit in 
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+
+                    <p>Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                         sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </InformationModal>
-                <InformationModal 
-                    open={this.state.showDsgvo} 
+                <InformationModal
+                    open={this.state.showDsgvo}
                     toggle={() => this.toggleDsgvo()}
                     headline="Datenschutz">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        
-                        <p>Duis aute irure dolor in reprehenderit in 
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+
+                    <p>Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                         sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </InformationModal>
-                <InformationModal 
-                    open={this.state.showAbout} 
+                <InformationModal
+                    open={this.state.showAbout}
                     toggle={() => this.toggleAbout()}
                     headline="Über die Seite">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                        
-                        <p>Duis aute irure dolor in reprehenderit in 
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <b>Das Spiel</b>
+                    <p>Das Spiel habe ich als historisches Spiel (kleine Mühle) bzw als Variation der runden römischen Mühle
+                        im Netz gefunden. Es ist wohl ein Vorlgänger der heutigen Mühlespiele und des Tic Tac Toe.
+                        </p>
+
+                    <b>Implementierung</b>
+                    <p>Das gesamte Spiel ist in Typescript programmiert und läuft komplett im Browser
+                        (keine weiteren Serveranfragen, während des Spiels).
+                            Außerdem verwende ich React für den Aufbau der Seite.</p>
+
+                    <b>Spiel KI</b>
+                    <p>Initial kennt die KI nur die Zielzustände - also die Positionen auf dem Spielfeld die dazu führen, dass ein
+                        Spieler gewinnt. Die Schwierigkeitslevel unterscheiden sich darin, wie weit die KI 'voraus denkt'.
+                        </p>
+                    <p>
+                        <ul>
+                            <li>Einfach - 1 Runde, also eigener Zug und idealer Zug des Gegners werden berücksichtigt</li>
+                            <li>Normal - 2 Runden</li>
+                            <li>Schwierig - 3 Runden</li>
+                        </ul>
+                    </p>
+                    <p>
+                        Da das Spielfeld nicht sehr komplex ist funktioniert das bereits recht vernünftig.
+                            </p>
+                    <p>
+                        Wenn ihr Spieler 1 und 2 vom Computer steuern lasst, könnt ihr die die KI aber zusätzlich trainieren.
+                        Dabei spielt der Computer 1000x gegen sich selbst und sammelt Daten. Danach wird das Spiel tatsächlich noch
+                        etwas herausfordernder.
+                        </p>
                 </InformationModal>
             </Navbar>)
     }
